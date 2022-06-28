0bf3e2b7389928fcaed0e3896e90b0dd4082da98
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-06-28 12:13:13 -0300

    Funcionalidade de excluir personalidade adicionada

--- routes/routes.go
+++ routes/routes.go
@@ -11,9 +11,10 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
-	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("GET")
-	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("POST")
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
